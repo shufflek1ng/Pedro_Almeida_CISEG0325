00245033d34a62bbdff95473f0bf8a87c85c124f
Pedro Almeida <pedrofbalmeida91@gmail.com>
2025-04-04 19:53:14 +0100

    6

--- Pedro_Almeida_CISEG0325_5117/Exercícios_IF/Exercício_6.py
+++ Pedro_Almeida_CISEG0325_5117/Exercícios_IF/Exercício_6.py
@@ -3,9 +3,11 @@ cliente=(input("Digite o seu nome: "))
 
 compra=float(input("Qual é o valor da compra? : € "))
 
-if compra <= 200:
+if compra <= 200 and compra > 0:
     print(f"{cliente} a sua compra no valor de {compra}€ tem um desconto de 10% no valor de {compra - (compra * 0.10)}€")
-elif compra > 200 and compra <= 500:
+elif compra > 200 and compra <= 500 and compra > 0:
     print(f"{cliente} a sua compra no valor de {compra}€ tem um desconto de 15% no valor de {compra - (compra * 0.15)}€")
-else:
+elif compra > 500 and compra > 0:
     print(f"{cliente} a sua compra no valor de {compra}€ tem um desconto de 20% no valor de {compra - (compra * 0.20)}€")
+else:
+    print(f"O cliente {cliente} não efectou nenhuma compra")
