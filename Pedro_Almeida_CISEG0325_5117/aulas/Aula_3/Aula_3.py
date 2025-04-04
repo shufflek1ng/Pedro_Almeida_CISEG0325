d93407311719dd704e9db19ee332d2f14f7047c6
Pedro Almeida <pedrofbalmeida91@gmail.com>
2025-04-04 21:25:17 +0100

    7

---
+++ Pedro_Almeida_CISEG0325_5117/aulas/Aula_3/Aula_3.py
@@ -0,0 +1,13 @@
+numeros=["um", "dois", "três", "quatro"] # = 4
+     # [  0,  1,  2,  3  ]
+#for numero in numeros: -> obriga a itenerar todas as posições da lista
+#
+#for index in range(4):
+#        if(index%2 == 0):
+ #           print(f"index = {index} valor na posição = {numeros[index]}")
+
+i=0
+#condicional continua se for "True"
+while i<4 and i>7: #Faz loop enquanto for "True"
+    print("i = ",i, "valor na posição =", numeros[i])
+    i+=1
